372f25706a2a6a88dbf2beb6308e4d13c4f64a56
Brabec, Michal <michal.brabec@hidglobal.com>
2025-07-19 19:34:36 +0200

    Hardhat tutorial, react app

--- SimpleStorage/deployEncrypted.ts
+++ SimpleStorage/deployEncrypted.ts
@@ -8,12 +8,11 @@ import {
     Provider,
     BaseContract,
     TransactionResponse,
-    Typed
+    Typed,
+    ethers
 } from "ethers"
 import {config} from "dotenv"
 import {promises} from "fs"
-import { Contract } from "ethers"
-import { BigNumber } from "ethers-ts"
 
 async function main(): Promise<void> {
     // setup
@@ -24,38 +23,43 @@ async function main(): Promise<void> {
     if(!process.env.RPC_URL) {
         throw "RPC URL missing"
     }
+    if(!process.env.PASSWORD) {
+        throw "Password missing"
+    }
     console.log(process.env.PRIVATE_KEY)
     console.log(process.env.RPC_URL)
+    console.log(process.env.PASSWORD)
 
     const abi: string = await promises.readFile('./SimpleStorage_sol_SimpleStorage.abi', {encoding: 'utf8'})
     const bin: string = await promises.readFile('./SimpleStorage_sol_SimpleStorage.bin', {encoding: 'utf8'})
     
     // ethers setup
     const provider: Provider = new JsonRpcProvider(process.env.RPC_URL)
-    const wallet: Wallet = new Wallet(process.env.PRIVATE_KEY, provider)
+    const wallet: Wallet = new Wallet(process.env.PRIVATE_KEY)
     console.log(wallet)
+    const encrypted: string = await wallet.encrypt(process.env.PASSWORD)
+    console.log(encrypted)
+    await promises.writeFile('./private_key.json', encrypted)
+
+    const encryptedKey:string = await promises.readFile('./private_key.json', {encoding: 'utf8'})
+    const encryptedWallet: Wallet = Wallet.fromEncryptedJsonSync(encryptedKey, process.env.PASSWORD) as Wallet
+    console.log(encryptedWallet)
+    encryptedWallet.connect(provider)
+    console.log(encryptedWallet)
 
     // ethers contract setup
-    const factory: ContractFactory<number[], BaseContract> = new ContractFactory(abi, bin, wallet)
+    const factory: ContractFactory<number[], BaseContract> = new ContractFactory(abi, bin, encryptedWallet)
     console.log(factory)
-    const contract: BaseContract & { 
-        addUser?: (name: string, number: number) => TransactionResponse, 
-        getNumber?: (name: Typed) => BigInt
-    } = await factory.deploy(10); // Contract constructor parameters
+    const contract: any = await factory.deploy(10); // Contract constructor parameters
     console.log(contract)
     let response: TransactionResponse | null = contract.deploymentTransaction()
     console.log(response)
     let receipt: TransactionReceipt | undefined | null = await response?.wait(1)
     console.log(receipt)
 
-    // console.log('---------------------------------')
-    // const tx: ContractDeployTransaction = await factory.getDeployTransaction(10); // create contract transaction data encoding
-    // console.log(tx) // the transaction parameters are appended to the contract bytecode or other data
-    // console.log('---------------------------------')
-
     // manual contract setup
     const transaction: TransactionRequest = {
-        nonce: await provider.getTransactionCount(wallet.address),
+        nonce: await provider.getTransactionCount(encryptedWallet.address),
         gasPrice: 20000000000,
         gasLimit: 1000000,
         to: null,
@@ -64,18 +68,18 @@ async function main(): Promise<void> {
         chainId: 1337
     }
     console.log(transaction)
-    const signed: string = await wallet.signTransaction(transaction)
+    const signed: string = await encryptedWallet.signTransaction(transaction)
     console.log(signed)
-    response = await wallet.sendTransaction(transaction)
+    response = await encryptedWallet.sendTransaction(transaction)
     console.log(response)
     receipt = await response.wait(1)
     console.log(receipt)
 
     // add user and get taht users number
-    const newUser: TransactionResponse = await contract.addUser!("joe", 11)
+    const newUser = await contract.addUser("joe", 11)
     console.log(newUser)
     console.log(await newUser.wait(1)) // adding user is a transaction so it must be resolved
-    const userNumber: BigInt = await contract.getNumber!(Typed.string('joe'))
+    const userNumber = await contract.getNumber(Typed.string('joe'))
     console.log(userNumber) // getting the user number is a view function which does not need resolve
 }
 
